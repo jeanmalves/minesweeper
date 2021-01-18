6e65b9108e3e10e663bc346cea85e1bcafdd50ee
Jeam Alves <jeanmalves@gmail.com>
2021-01-18 20:19:51 -0300

    Created game map and each square to area

---
+++ src/components/GameMap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="row">
+      <Square />
+    </div>
+    <div class="row">
+      <Square />
+    </div>
+    <div class="row">
+      <Square />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Square from "@/components/Square.vue";
+
+@Component({
+  components: {
+    Square
+  }
+})
+export default class GameMap extends Vue {}
+</script>
+<style scoped>
+.row {
+  width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="square"></div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Square extends Vue {}
+</script>
+<style scoped>
+    .square {
+        margin: 1px;
+        width:50px;
+        height: 50px;
+        flex-grow: 1;
+        background-color: #aeb0bd;
+    }
+</style>
