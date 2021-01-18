304a975ce870c86b54e89c7ca737669c995197df
Jeam Alves <jeanmalves@gmail.com>
2021-01-18 20:18:40 -0300

    Page game structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Map />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import GameMap from "@/components/GameMap.vue";
 
 @Component({
   components: {
-    HelloWorld
+    GameMap
   }
 })
 export default class Home extends Vue {}
