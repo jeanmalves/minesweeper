f8495fa83b203e4fccc6f532c815e7321f0857fd
Jeam Alves <jeanmalves@gmail.com>
2021-01-20 00:14:43 -0300

    Added classes of the game

---
+++ src/model/game-map.ts
@@ -0,0 +1,7 @@
+import { Square } from "./square";
+
+export class GameMap {
+  size = 10;
+  totalMines = 10;
+  square: Square[][] = [];
+}
---
+++ src/model/square.ts
@@ -0,0 +1,7 @@
+export class Square {
+  value: number;
+
+  constructor() {
+    this.value = SquareValue.EMPTY;
+  }
+}
