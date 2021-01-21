bfecdd4cd5c5e2f4e34421433f74329b41247654
Jeam Alves <jeanmalves@gmail.com>
2021-01-20 21:43:20 -0300

    Added state and getter for game store

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,4 +1,11 @@
+import { GameMap } from "@/model/game-map";
 import { Module, VuexModule } from "vuex-module-decorators";
 
 @Module({ name: "GameStore" })
-export class GameStore extends VuexModule {}
+export class GameStore extends VuexModule {
+  gameMapState = new GameMap();
+
+  get gameMap() {
+    return this.gameMapState;
+  }
+}
