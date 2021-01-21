235c7a11fa762a4b14012dd37301e446a616fdf8
Jeam Alves <jeanmalves@gmail.com>
2021-01-20 21:45:21 -0300

    Create props and render square component

--- src/components/GameMap.vue
+++ src/components/GameMap.vue
@@ -1,27 +1,25 @@
 <template>
   <div>
-    <div class="row">
-      <Square />
-    </div>
-    <div class="row">
-      <Square />
-    </div>
-    <div class="row">
-      <Square />
+    <div class="row" v-for="(size, row) in squares" :key="`row-${row}`">
+      <square-component v-for="(size, column) in size" :key="`col-${column}`" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Square from "@/components/Square.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import SquareComponent from "@/components/Square.vue";
+import { Square } from "@/model/square";
 
 @Component({
   components: {
-    Square
+    SquareComponent
   }
 })
-export default class GameMap extends Vue {}
+export default class GameMapComponent extends Vue {
+  @Prop() readonly squares!: Square[][];
+  @Prop() readonly size!: number;
+}
 </script>
 <style scoped>
 .row {
