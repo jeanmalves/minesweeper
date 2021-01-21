7ae0c49c8c3fccb4fb21fab525b3ba361b7231c1
Jeam Alves <jeanmalves@gmail.com>
2021-01-20 21:42:35 -0300

    Initialize game map with default value for square

--- src/model/game-map.ts
+++ src/model/game-map.ts
@@ -3,5 +3,18 @@ import { Square } from "./square";
 export class GameMap {
   size = 10;
   totalMines = 10;
-  square: Square[][] = [];
+  squares: Square[][] = [];
+
+  constructor() {
+    this.initialize();
+  }
+
+  private initialize() {
+    for (let row = 0; row < this.size; row++) {
+      this.squares[row] = [];
+      for (let column = 0; column < this.size; column++) {
+        this.squares[row][column] = new Square(row, column);
+      }
+    }
+  }
 }
