fcc840a4d7fca19b49694919032299959034e619
Jeam Alves <jeanmalves@gmail.com>
2021-01-20 21:44:35 -0300

    Set state game and render game map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
-    <GameMap />
+    <game-map-component :squares="gameMap.squares" :size="gameMap.size" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import GameMap from "@/components/GameMap.vue";
+import { Getter } from "vuex-class";
+import GameMapComponent from "@/components/GameMap.vue";
+import { GameMap } from "@/model/game-map";
 
 @Component({
   components: {
-    GameMap
+    GameMapComponent
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Getter gameMap!: GameMap;
+}
 </script>
